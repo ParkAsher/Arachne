1b7b731f5986d3440bc51eecb7e3f935b7820c7a
ParkAsher <devasherpark@naver.com>
2023-04-11 22:03:22 +0900

    [Refactor]: 회원가입 데이터 변수명 수정

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,29 +1,27 @@
 function signup() {
-    const userId = document.getElementById('user-id').value; // 아이디
-    const userPassword = document.getElementById('user-password').value; // 비밀번호
-    const userPasswordCheck = document.getElementById(
-        'user-password-check',
-    ).value; // 비밀번호확인
-    const userName = document.getElementById('user-name').value; // 이름
-    const userEmail = document.getElementById('user-email').value; // 이메일
-    const userPhone = document.getElementById('user-phone').value; // 휴대폰번호
-    const userNickname = document.getElementById('user-nickname').value; // 닉네임
+    const id = document.getElementById('user-id').value; // 아이디
+    const password = document.getElementById('user-password').value; // 비밀번호
+    const passwordCheck = document.getElementById('user-password-check').value; // 비밀번호확인
+    const name = document.getElementById('user-name').value; // 이름
+    const email = document.getElementById('user-email').value; // 이메일
+    const phone = document.getElementById('user-phone').value; // 휴대폰번호
+    const nickname = document.getElementById('user-nickname').value; // 닉네임
 
     if (
-        !userId ||
-        !userPassword ||
-        !userPasswordCheck ||
-        !userName ||
-        !userEmail ||
-        !userPhone ||
-        !userNickname
+        !id ||
+        !password ||
+        !passwordCheck ||
+        !name ||
+        !email ||
+        !phone ||
+        !nickname
     ) {
         alert('빈칸을 모두 입력해주세요.');
         return;
     }
 
     // 비밀번호 확인
-    if (userPassword !== userPasswordCheck) {
+    if (password !== passwordCheck) {
         // 비밀번호 확인 입력칸 비우기
         let errorMessage = `<div class="signup-error-message">비밀번호가 일치하지 않습니다.</div>`;
         document
@@ -35,12 +33,12 @@ function signup() {
     }
 
     const userInfo = {
-        userId,
-        userPassword,
-        userName,
-        userEmail,
-        userPhone,
-        userNickname,
+        id,
+        password,
+        name,
+        email,
+        phone,
+        nickname,
     };
 
     axios
--- src/user/dto/signup-user.dto.ts
+++ src/user/dto/signup-user.dto.ts
@@ -4,28 +4,28 @@ export class signupUserDto {
     @Matches(/^(?=.*[a-z])(?=.*\d)[a-z\d]{5,15}$/, {
         message: 'id',
     })
-    userId: string;
+    id: string;
 
     @Matches(
         /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/,
         { message: 'password' },
     )
-    userPassword: string;
+    password: string;
 
     @Matches(/^[\u1100-\u11FF\u3130-\u318F\uAC00-\uD7AF]{2,20}$/, {
         message: 'name',
     })
-    userName: string;
+    name: string;
 
     @IsEmail({}, { message: 'email' })
-    userEmail: string;
+    email: string;
 
     @Matches(/^[\u1100-\u11FF\u3130-\u318F\uAC00-\uD7AFa-z\d]{2,15}$/, {
         message: 'nickname',
     })
-    userNickname: string;
+    nickname: string;
 
     @MinLength(10, { message: 'phone' })
     @MaxLength(11, { message: 'phone' })
-    userPhone: string;
+    phone: string;
 }
