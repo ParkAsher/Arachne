9cf7a766d6a95324257bbf5e996ab4ea551bb357
ParkAsher <devasherpark@naver.com>
2023-04-13 00:45:45 +0900

    [Chore]: Json Web Token 설정

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from '../entities/users.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtConfigService } from 'src/config/jwt.config.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Users])],
+    imports: [
+        TypeOrmModule.forFeature([Users]),
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            useClass: JwtConfigService,
+            inject: [ConfigService],
+        }),
+    ],
     controllers: [UserController],
     providers: [UserService],
 })
