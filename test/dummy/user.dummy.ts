e79c52be508846e5af4c8d172c402c919d561143
91song4 <91song4@daum.net>
2023-04-13 00:16:05 +0900

    [Test]: 유저 상세보기 e2e 테스트 추가

---
+++ test/dummy/user.dummy.ts
@@ -0,0 +1,24 @@
+export const UserDummy = [
+    {
+        userId: 1,
+        id: 'admin',
+        password: '123',
+        name: '관리자',
+        email: 'admin@admin.com',
+        nickname: 'adminNickname',
+        phone: '010-0000-0000',
+        profileImg: 'default.bmp',
+        role: 1,
+    },
+    {
+        userId: 2,
+        id: 'song',
+        password: '123',
+        name: '송지훈',
+        email: 'song@gmail.com',
+        nickname: 'song4',
+        phone: '010-1111-1111',
+        profileImg: 'default.bmp',
+        role: 2,
+    },
+];
