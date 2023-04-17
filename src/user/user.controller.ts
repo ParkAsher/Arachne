7da21ff34ee5ebd53cd1c50f799bea71ba2406f3
ParkAsher <devasherpark@naver.com>
2023-04-17 22:33:13 +0900

    [Feat]: 로그아웃 API 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,6 +7,7 @@ import {
     Post,
     Req,
     Res,
+    UnauthorizedException,
     UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
@@ -15,10 +16,14 @@ import { signinUserDto } from './dto/signin-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Users } from 'src/entities/users.entity';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { CacheService } from 'src/cache/cache.service';
 
 @Controller('/api/users')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+    constructor(
+        private readonly userService: UserService,
+        private readonly cacheService: CacheService,
+    ) {}
 
     // 로그인 유무
     @UseGuards(AuthGuard)
@@ -29,16 +34,34 @@ export class UserController {
         return { isLoggedIn, userInfo };
     }
 
-    @Get('/:userId')
-    async getUser(@Param('userId') userId: number): Promise<Users> {
-        return await this.userService.getUser(userId);
+    // 로그아웃
+    @UseGuards(AuthGuard)
+    @Get('/signout')
+    async signOut(@Req() req, @Res() res) {
+        const { isLoggedIn, userInfo } = req.auth;
+
+        // 이미 로그아웃 상태라면 불가능한 기능
+        if (!isLoggedIn) {
+            throw new UnauthorizedException('로그인 중이 아닙니다.');
+        }
+
+        // Access Token, Refresh Token 다 지우기
+        res.clearCookie('accessToken');
+        res.clearCookie('refreshToken');
+
+        // Redis Refresh Token 지우기
+        await this.cacheService.removeRefreshToken(userInfo.userId);
+
+        return res.send();
     }
 
+    // 회원 가입
     @Post('/signup')
     async signUp(@Body() userInfo: signupUserDto) {
         return await this.userService.signUpUser(userInfo);
     }
 
+    // 로그인
     @Post('/signin')
     async signIn(@Body() userInfo: signinUserDto, @Res() res) {
         const { accessToken, refreshToken } = await this.userService.signInUser(
@@ -53,6 +76,11 @@ export class UserController {
         });
 
         return res.send();
+    }
+
+    @Get('/:userId')
+    async getUser(@Param('userId') userId: number): Promise<Users> {
+        return await this.userService.getUser(userId);
     }
 
     @Patch('/:userId')
