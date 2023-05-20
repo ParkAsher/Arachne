c2eb2dce775e1614ab82372187470290c657c66d
91song4 <91song4@daum.net>
2023-05-20 13:43:19 +0900

    [Test]: 인증번호 확인 e2e테스트 추가

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -6,6 +6,9 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { AuthModule } from 'src/auth/auth.module';
+import { CheckAuthCodeDto } from 'src/auth/dto/check-auth-code.dto';
+import { MailerModule } from '@nestjs-modules/mailer';
+import axios from 'axios';
 
 describe('AuthController (e2e)', () => {
     let app: INestApplication;
@@ -33,6 +36,20 @@ describe('AuthController (e2e)', () => {
                     synchronize: true,
                     logging: false,
                 }),
+                MailerModule.forRoot({
+                    transport: {
+                        host: process.env.MAILTRAP_HOST,
+                        port: +process.env.MAILTRAP_PORT,
+                        auth: {
+                            user: process.env.MAILTRAP_USER,
+                            pass: process.env.MAILTRAP_PASS,
+                        },
+                    },
+                    defaults: {
+                        from: process.env.MAILTRAP_FROM,
+                        name: process.env.MAILTRAP_NAME,
+                    },
+                }),
                 AuthModule,
             ],
         })
@@ -58,13 +75,38 @@ describe('AuthController (e2e)', () => {
         it('비밀번호 찾기 - 인증메일 보내기', async () => {
             // Given
             const url = '/api/auth/send-auth-code';
-            const body = { email: 'freersong@gmail.com' };
+            const body = { email: UserDummy[0].email };
 
             // When
             const res = await server.post(url).send(body);
 
             // Then
-            expect(1 + 1).toBe(2);
+            expect(res.status).toBe(201);
+        });
+    });
+
+    describe('POST /api/auth/check-auth-code', () => {
+        it('비밀번호 찾기 - 인증번호 확인', async () => {
+            // Given
+            const url = '/api/auth/check-auth-code';
+            const { data } = await axios.get(
+                `https://mailtrap.io/api/accounts/${process.env.MAILTRAP_ACCOUNT_ID}/inboxes/${process.env.MAILTRAP_INBOX_ID}/messages`,
+                {
+                    headers: {
+                        Accept: 'application/json',
+                        'Api-Token': process.env.MAILTRAP_API_TOKEN,
+                    },
+                },
+            );
+            const checkAuthCodeDto: CheckAuthCodeDto = {
+                email: UserDummy[0].email,
+                authCode: +data[0].subject.split(':')[1].trim(),
+            };
+
+            // When
+            const res = await server.post(url).send(checkAuthCodeDto);
+
+            // Then
             expect(res.status).toBe(201);
         });
     });
