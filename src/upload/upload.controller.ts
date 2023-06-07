f09c73ec7519b457fa2f7bb5d832f12c46bd2a79
Jeongjiw00 <wldn0229@naver.com>
2023-06-07 18:55:43 +0900

    [Feat]: 백오피스-유저 이미지업로드 기능 추가

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,5 +1,7 @@
 import {
     Controller,
+    Param,
+    Patch,
     Post,
     Req,
     UnauthorizedException,
@@ -29,6 +31,24 @@ export class UploadController {
             throw new UnauthorizedException('로그인 중이 아닙니다.');
         }
 
+        // S3 프로필 이미지 업로드
+        const profileImagePath = await this.uploadService.uploadProfileImage(
+            file,
+            'profile',
+        );
+
+        // 회원 프로필이미지 링크 수정
+        this.userService.updateUserProfileImage(userId, profileImagePath);
+
+        return { profileImagePath };
+    }
+
+    @Patch('/admin/profile/:userId')
+    @UseInterceptors(FileInterceptor('file'))
+    async adminUploadFile(
+        @UploadedFile() file: Express.MulterS3.File,
+        @Param('userId') userId: number,
+    ) {
         // S3 프로필 이미지 업로드
         const profileImagePath = await this.uploadService.uploadProfileImage(
             file,
