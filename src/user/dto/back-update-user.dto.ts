ac4413fabf542d1aeff79f2c99640b2ef0593098
Jeongjiw00 <wldn0229@naver.com>
2023-06-07 18:54:27 +0900

    [Refactor] dto 수정

--- src/user/dto/back-update-user.dto.ts
+++ src/user/dto/back-update-user.dto.ts
@@ -1,34 +1,42 @@
 import {
     IsEmail,
-    IsString,
     MaxLength,
     MinLength,
     IsNumber,
+    Matches,
+    IsOptional,
 } from 'class-validator';
 
 export class BackUpdateUserDto {
-    @IsString()
-    id?: string;
+    @Matches(/^(?=.*[a-z])(?=.*\d)[a-z\d]{5,15}$/, {
+        message: 'id',
+    })
+    id: string;
 
-    @IsString()
+    @IsOptional()
+    @Matches(
+        /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/,
+        { message: 'password' },
+    )
     password?: string;
 
-    @IsString()
-    name?: string;
-
-    @IsString()
-    nickname?: string;
-
-    @IsString()
-    profileImg?: string;
+    @Matches(/^[\u1100-\u11FF\u3130-\u318F\uAC00-\uD7AF]{2,20}$/, {
+        message: 'name',
+    })
+    name: string;
 
     @IsEmail({}, { message: 'email' })
-    email?: string;
+    email: string;
+
+    @Matches(/^[\u1100-\u11FF\u3130-\u318F\uAC00-\uD7AFa-z\d]{2,15}$/, {
+        message: 'nickname',
+    })
+    nickname: string;
 
     @MinLength(10, { message: 'phone' })
     @MaxLength(11, { message: 'phone' })
-    phone?: string;
+    phone: string;
 
     @IsNumber()
-    role?: number;
+    role: number;
 }
